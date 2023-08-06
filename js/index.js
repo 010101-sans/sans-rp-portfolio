1f991a7a00d673921fd89de2abf503c70667f815
Sanskar Suryawanshi <sanskarsuryawanshi123@gmail.com>
2023-08-06 11:53:09 +0530

    Added some new features to the footer section

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 /* ------------- Book Images Horizontal Scroll ------------- */
 
 // Get the books container and initial mouse position
-const booksContainer = document.querySelector('.books-container');
+const booksContainer = document.querySelector(".books-container");
 let isDragging = false;
 let startX;
 let scrollLeft;
 
 // Event listeners for mouse events
-booksContainer.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.pageX - booksContainer.offsetLeft;
-    scrollLeft = booksContainer.scrollLeft;
+booksContainer.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.pageX - booksContainer.offsetLeft;
+  scrollLeft = booksContainer.scrollLeft;
 });
 
-booksContainer.addEventListener('mouseleave', () => {
-    isDragging = false;
+booksContainer.addEventListener("mouseleave", () => {
+  isDragging = false;
 });
 
-booksContainer.addEventListener('mouseup', () => {
-    isDragging = false;
+booksContainer.addEventListener("mouseup", () => {
+  isDragging = false;
 });
 
-booksContainer.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    e.preventDefault();
+booksContainer.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
   const x = e.pageX - booksContainer.offsetLeft;
   const walk = (x - startX) * 2; // Control the scroll speed here
   booksContainer.scrollLeft = scrollLeft - walk;
@@ -31,21 +31,68 @@ booksContainer.addEventListener('mousemove', (e) => {
 
 /* ------------- For Query Handling ------------- */
 
-function submitForm() {
-    // Get the values from the input fields
-    const name = document.getElementById('student-name').value;
-    const college = document.getElementById('student-college').value;
-    const phoneNumber = document.getElementById('student-phone').value;
-    const subjects = document.getElementById('subjects').value;
-    const classShift = document.getElementById('class-shift').value;
-    const message = document.getElementById('message').value;
+function sendEmail() {
+    // Extract form data
+    var name = document.getElementById("nameInput").value;
+    var college = document.getElementById("collegeInput").value;
+    var phone = document.getElementById("phoneInput").value;
+    var subjects = document.getElementById("subjectsInput").value;
+    var message = document.getElementById("messageInput").value;
+    
+    // Format the email message
+    var emailMessage =
+    "Name: " + name + "\n" +
+    "College: " + college + "\n" +
+    "Phone Number: " + phone + "\n" +
+    "Subjects: " + subjects + "\n";
+    
+    if (message) { emailMessage += "Message: " + message + "\n"; }
+    
+    // Construct the mailto link
+    var emailAddress = "contactsanskarcs@gmail.com";
+    var subject = "Query from Website";
+    var mailtoLink =
+    "mailto:" + emailAddress +
+    "?subject=" + encodeURIComponent(subject) +
+    "&body=" + encodeURIComponent(emailMessage);
+    
+  // Create a hidden anchor element and set its attributes
+  var mailtoAnchor = document.createElement("a");
+  mailtoAnchor.setAttribute("href", mailtoLink);
+  mailtoAnchor.style.display = "none"; // Hide the anchor element
+  
+  // Add the anchor element to the document body
+  document.body.appendChild(mailtoAnchor);
+  
+  // Trigger the click event on the hidden anchor element
+  mailtoAnchor.click();
+  
+  // Remove the anchor element from the document body (optional)
+  document.body.removeChild(mailtoAnchor);
+  
+  // Reset the form fields (optional)
+  document.getElementById("nameInput").value = "";
+  document.getElementById("collegeInput").value = "";
+  document.getElementById("phoneInput").value = "";
+  document.getElementById("subjectsInput").value = "";
+  document.getElementById("messageInput").value = "";
+}
 
-    // Compose the email body
-    const body = `Student Name: ${name}\nStudent College: ${college}\nPhone Number: ${phoneNumber}\nSubjects: ${subjects}\nClass Shift: ${classShift}\n\nMessage: ${message}`;
+/* ------------- slide-show ------------- */
 
-    // Create the mailto link with the email address and subject
-    const mailtoLink = `mailto:contactsanskarcs@gmail.com?subject=Class%20Query&body=${encodeURIComponent(body)}`;
+const slideshow = document.querySelector(".slideshow");
+const images = slideshow.querySelectorAll("img");
+let currentIndex = 0;
 
-    // Open the mail client with the pre-filled email data
-    window.location.href = mailtoLink;
-  }
+function startSlideshow() {
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % images.length;
+        updateSlide();
+    }, 3000); // Change the time (in milliseconds) to adjust the slide duration
+}
+
+function updateSlide() {
+    slideshow.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+startSlideshow();
